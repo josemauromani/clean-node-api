bc21b34b59608e7a899d0bc15dded8b9f2107c6a
Jose Mauro <jose.mani@oficinacerta.app>
2023-01-03 17:33:48 -0300

    adds usecase and account repository

--- login.js
+++ login.js
@@ -1,15 +1,34 @@
-const express = require('express')
-const router = express.Router()
-const mongoose = require('mongoose')
-const AccountModel = mongoose.model('Account')
 
 module.exports = () => {
-    router.post('/signup', async (req, res) => {
+    router.post('/signup', new SignUpRouter().route)
+}
+
+// signup-router.js
+const express = require('express')
+const router = express.Router()
+class SignUpRouter {
+    async route(req, res) {
         const { email, password, repeatPassword } = req.body
-        if (password === repeatPassword) {
-            const user = await AccountModel.create(email, password)
-            return res.json(user)
-        }
+        SignUpUseCase.signup(email, password, repeatPassword)
         res.status(400).json({ error: 'password must be equal to repeat password' })
-    })
+    }
+}
+
+// signup-case.js
+class SignUpUseCase {
+    async signup(email, password, repeatPassword) {
+        if (password === repeatPassword) {
+            AddAccountRepository.add(email, password)
+        }
+    }
+}
+
+// add-account-repository.js
+const mongoose = require('mongoose')
+const AccountModel = mongoose.model('Account')
+class AddAccountRepository {
+    async add(email, password) {
+        const user = await AccountModel.create(email, password)
+        return user
+    }
 }
