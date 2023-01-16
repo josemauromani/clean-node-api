2dcfdef91c1fd0ea1dc5d017511005e75fa5f015
Jose Mauro <jose.mani@oficinacerta.app>
2023-01-16 11:54:31 -0300

    finish class #2

--- login.js
+++ login.js
@@ -1,19 +1,55 @@
-
-module.exports = () => {
-    router.post('/signup', new SignUpRouter().route)
-}
-
-// signup-router.js
 const express = require('express')
 const router = express.Router()
-class SignUpRouter {
-    async route(req, res) {
-        const { email, password, repeatPassword } = req.body
-        SignUpUseCase.signup(email, password, repeatPassword)
-        res.status(400).json({ error: 'password must be equal to repeat password' })
+
+/**
+ * Codigo Original
+ */
+
+// module.exports = () => {
+//     router.post('signup', async (req, res) => {
+//         const { email, password, repeatPassword } = req.body
+//         if (password === repeatPassword) {
+//             const user = await AccountModel.create({ email, password })
+//             return res.json(user)
+//         }
+//         res.status(400).json({ error: 'password must be equal to repeatPassword' })
+//     })
+// }
+
+module.exports = () => {
+    const signUpRouter = new SignUpRouter()
+    router.post('/signup', ExpressRouterAdapter.adapt(signUpRouter))
+}
+
+// Adapter
+// express-router-adapter.js
+class ExpressRouterAdapter {
+    static adapt(router) {
+        let httpRequest
+        return async (req, res) => {
+            httpRequest = {
+                body: req.body
+            }
+            const httpResponse = await router.route(httpRequest)
+            res.status(httpResponse.statusCode).json(httpResponse.body)
+        }
     }
 }
 
+// Presentation Layer
+// signup-router.js
+class SignUpRouter {
+    async route(httpRequest) {
+        const { email, password, repeatPassword } = httpRequest.body
+        const user = await SignUpUseCase.signup(email, password, repeatPassword)
+        return {
+            statusCode: 200,
+            body: user
+        }
+    }
+}
+
+// Domain Layer
 // signup-case.js
 class SignUpUseCase {
     async signup(email, password, repeatPassword) {
@@ -23,6 +59,7 @@ class SignUpUseCase {
     }
 }
 
+// Infra Layer
 // add-account-repository.js
 const mongoose = require('mongoose')
 const AccountModel = mongoose.model('Account')
